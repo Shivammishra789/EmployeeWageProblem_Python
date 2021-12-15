c3512ff354caf006fe6a4a5056b553762a285ae5
Shivammishra789 <shivammishra7789@gmail.com>
2021-12-16 00:09:31 +0530

    ADDED: employee wage calculated till condition of 20 days or 100 hours is reached

--- employee_payroll_sys.py
+++ employee_payroll_sys.py
@@ -1,12 +1,16 @@
-'''Calculating monthly employee wage for full time, part time or absent for 20 days'''
+'''Calculating monthly employee wage till 20 days or 100 hours are reached'''
 import random
 
 WAGE_PER_HOUR = 20
+MONTHLY_WORKING_DAYS = 20
+MONTHLY_WORKING_HRS = 100
 FULL_TIME = 8
 PART_TIME = 4
 ABSENT = 0
 emp_hrs = 0
 monthly_emp_wage = 0
+emp_working_days = 0
+emp_working_hrs = 0
 
 
 def random_employee_presentee():
@@ -21,10 +25,17 @@ def random_employee_presentee():
         emp_hrs = ABSENT
 
 
-# getting random employee presentee for 20 days
-for x in range(0, 20):
+# loop runs till monthly working days 20 or monthly working hrs 100 is reached
+while emp_working_days < MONTHLY_WORKING_DAYS and emp_working_hrs <= MONTHLY_WORKING_HRS:
+    emp_working_days += 1
+    if emp_working_hrs > 100:
+        emp_working_hrs -= emp_hrs
+        break
     # calling function
     random_employee_presentee()
-    daily_emp_wage = WAGE_PER_HOUR * emp_hrs
-    monthly_emp_wage += daily_emp_wage
+    emp_working_hrs += emp_hrs
+    print('Day:', emp_working_days, 'Hrs Present:', emp_hrs)
+    monthly_emp_wage = WAGE_PER_HOUR * emp_working_hrs
+print('Total working hours:', emp_working_hrs)
 print('Monthly employee wage is:', monthly_emp_wage)
+
