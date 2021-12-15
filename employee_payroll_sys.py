61d8c819dce00caad568b34c61f3b7dd1ad56b22
Shivammishra789 <shivammishra7789@gmail.com>
2021-12-15 22:05:39 +0530

    ADDED: part time hrs and calculated daily employee wage

--- employee_payroll_sys.py
+++ employee_payroll_sys.py
@@ -1,13 +1,19 @@
-'''Calculating daily employee wage '''
+'''Calculating daily employee wage for full time, part time or absent '''
 import random
 
 WAGE_PER_HOUR = 20
-FULL_DAY = 8
+FULL_TIME = 8
+PART_TIME = 4
 ABSENT = 0
+emp_hrs = 0
+
 # generating random number
-rand_num = (random.randint(0, 1))
+rand_num = (random.randint(0, 2))
+print(rand_num)
 if rand_num == 1:
-    emp_hrs = FULL_DAY
+    emp_hrs = FULL_TIME
+elif rand_num == 2:
+    emp_hrs = PART_TIME
 else:
     emp_hrs = ABSENT
 
